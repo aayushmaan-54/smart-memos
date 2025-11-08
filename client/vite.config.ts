148c6a7c2cea5995f03df1d8e9866a7369bae2c8
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2025-11-09 01:04:22 +0530

    WIP: Auth Frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
   server: {
-    port: 3000,
+    port: 5173,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
